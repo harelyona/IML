e8797528d7cb4532aa305dcc62e14ba9b51927c0
harel <harel.yona@mail.huji.ac.il>
2025-04-13 13:46:01 +0300

    start q6

--- 1/house_price_prediction.py
+++ 1/house_price_prediction.py
@@ -1,11 +1,30 @@
+import os
 from typing import NoReturn, Tuple
-
+from linear_regression import *
 import numpy as np
 import pandas as pd
 import random
 import matplotlib.pyplot as plt
 
 random.seed(1)
+
+
+def id_and_dates(X):
+    """drop ids and split dates"""
+    # Id is an irrelevant feature
+    # the condition  "id" in X.columns is True
+    if "id" in X.columns:
+        X = X.drop("id", axis=1)  # So why this line fails with KeyError: "['id'] not found in axis"????
+    # Split date into year and month features
+    if "date" in X.columns:
+        dates = X["date"]
+        X["year"] = dates.str[:4].astype("Int64")
+        X["month"] = dates.str[4:6].astype("Int64")
+        X["day"] = dates.str[6:8].astype("Int64")
+        X = X.drop("date", axis=1)
+    return X
+
+
 def preprocess_train(X: pd.DataFrame, y: pd.Series):
     """
     preprocess training data.
@@ -19,7 +38,19 @@ def preprocess_train(X: pd.DataFrame, y: pd.Series):
     -------
     A clean, preprocessed version of the data
     """
-    pass
+    X = id_and_dates(X)
+    # Get rid of nan samples
+    nan_indices_X = X.isna().any(axis=1)
+    nan_indices_y = y.isna()
+    combined_nan_indices = nan_indices_X | nan_indices_y
+
+    # Convert all elements to numeric values
+    for col in X.columns:
+        X[col] = pd.to_numeric(X[col],errors="raise")
+    y = pd.to_numeric(y, errors="raise")
+    X, y = X[~combined_nan_indices], y[~combined_nan_indices]
+
+    return X, y
 
 
 def preprocess_test(X: pd.DataFrame):
@@ -34,7 +65,16 @@ def preprocess_test(X: pd.DataFrame):
     -------
     A preprocessed version of the test data that matches the coefficients format.
     """
-    return
+    X = id_and_dates(X)
+    # Replace every nan element to the mean value of its column
+    for feature in X:
+        if feature == "date":
+            continue
+        mean = X[feature].mean()
+        X[feature] = X[feature].fillna(mean)
+    for col in X:
+        X[col] = pd.to_numeric(X[col], errors="raise")
+    return X
 
 
 def feature_evaluation(X: pd.DataFrame, y: pd.Series, output_path: str = ".") -> NoReturn:
@@ -54,88 +94,46 @@ def feature_evaluation(X: pd.DataFrame, y: pd.Series, output_path: str = ".") ->
     output_path: str (default ".")
         Path to folder in which plots are saved
     """
-    pass
+    X, response = preprocess_train(X, y)
+    pearson_correlations = {}
+    response_std = response.std()
+    for col in X.columns:
+        feature = X[col]
+        feature_std = feature.std()
+        cov = np.cov(feature, response)[0][1]
+
+        # Check for zero standard deviation to avoid division by zero
+        if feature_std > 0 and response_std > 0:
+            pearson = cov / (feature_std * response_std)
+            pearson_correlations[col] = pearson
+        else:
+            pearson_correlations[col] = 0
+
+        # Create plot
+        plt.title(f"house price vs {col}")
+        plt.scatter(feature, response)
+        plt.xlabel(col)
+        plt.ylabel("price")
+        plt.savefig(f"{output_path}{os.sep}{col}.png", format="png")
+        plt.clf()
+    return pd.Series(pearson_correlations)
 
 
 if __name__ == '__main__':
     df = pd.read_csv("house_prices.csv")
     df, y = df.drop("price", axis=1), df.price
-
+    output_path = "plots"
 
     # Question 2 - split train test
     m, n = df.shape
-    number_of_test_sampels = int(m * 0.25)
-    test_start = random.randint(0, int(m - number_of_test_sampels))
-    test_sample = df.iloc[test_start:test_start + number_of_test_sampels]
-    test_response = y.iloc[test_start:test_start + number_of_test_sampels]
-    X = df.drop(test_sample.index)
-    y = y.drop(test_sample.index)
+    test_data_size = int(m * 0.25)
+    training_data_size = m - test_data_size
+    test_start = random.randint(0, int(m - test_data_size))
+    test_sample = df.iloc[test_start:test_start + test_data_size]
+    test_response = y.iloc[test_start:test_start + test_data_size]
+    training_samples = df.drop(test_sample.index)
+    response = y.drop(test_sample.index)
 
-    # Question 3 - preprocessing of housing prices train dataset TODO complete this
-    def preprocess_data(X: pd.DataFrame, y: pd.Series)->Tuple[pd.DataFrame, pd.Series]:
-        X = id_and_dates(X)
-        nan_indices_X = X.isna().any(axis=1)
-        nan_indices_y = y.isna()
-        combined_nan_indices = nan_indices_X | nan_indices_y
-        return X[~combined_nan_indices], y[~combined_nan_indices]
-
-
-    def id_and_dates(X):
-        """drop ids and split dates"""
-        # Id is an irrelevant feature
-        # the condition  "id" in X.columns is True
-        if "id" in X.columns:
-            X = X.drop("id", axis=1)  # So why this line fails with KeyError: "['id'] not found in axis"????
-        # Split date into year and month features
-        if "date" in X.columns:
-            dates = X["date"]
-            X["year"] = dates.str[:4].astype("Int64")
-            X["month"] = dates.str[4:6].astype("Int64")
-            X["day"] = dates.str[6:8].astype("Int64")
-            X = X.drop("date", axis=1)
-        return X
-
-
-    # Question 4 - Feature evaluation of train dataset with respect to response
-    def feature_evaluation(X: pd.DataFrame, response: pd.Series) -> pd.Series:
-        X, response = preprocess_data(X, response)
-        pearson_correlations = {}
-        response_std = response.std()
-        for col in X.columns:
-            feature = X[col]
-            feature_std = feature.std()
-            cov = np.cov(feature, response)[0][1]
-
-            # Check for zero standard deviation to avoid division by zero
-            if feature_std > 0 and response_std > 0:
-                pearson = cov / (feature_std * response_std)
-                pearson_correlations[col] = pearson
-            else:
-                pearson_correlations[col] = 0
-
-            # Create plot
-            plt.title(f"house price vs {col}")
-            plt.scatter(feature, response)
-            plt.xlabel(col)
-            plt.ylabel("price")
-            plt.savefig(f"{col}.png", format="png")
-        return pd.Series(pearson_correlations)
-
-    # Question 5 - preprocess the test data
-    def preprocess_test_data(X:pd.DataFrame):
-        X = id_and_dates(X)
-        # Replace every nan element to the mean value of its column
-        X = X.drop("id", axis=1)
-        for feature in X:
-            if feature == "date":
-                continue
-            mean = X[feature].mean()
-            X[feature] = X[feature].fillna(mean)
-
-
-    print(feature_evaluation(X, y))
-    preprocess_test(test_sample)
-    #preprocess_test_data(test_sample)
     # Question 6 - Fit model over increasing percentages of the overall training data
     # For every percentage p in 10%, 11%, ..., 100%, repeat the following 10 times:
     #   1) Sample p% of the overall training data
@@ -143,3 +141,18 @@ if __name__ == '__main__':
     #   3) Test fitted model over test set
     #   4) Store average and variance of loss over test set
     # Then plot average loss as function of training size with error ribbon of size (mean-2*std, mean+2*std)
+    test_samples = preprocess_test(test_sample)
+    test_samples = test_samples.astype(np.float64)
+    for p in range(10, 101):
+        number_of_samples = int(training_data_size * p / 100)
+        pre_processed_X = training_samples.sample(number_of_samples)
+        y = response[pre_processed_X.index]
+        X, y = preprocess_train(pre_processed_X, y)
+        X = X.astype(np.float64)
+        y = y.astype(np.float64)
+        X = X.to_numpy()
+        y = y.to_numpy()
+        fit = LinearRegression(True)
+        fit.fit(X, y)
+
+
